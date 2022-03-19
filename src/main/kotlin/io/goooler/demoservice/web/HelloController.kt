1e8f5166e1d2edf9abd75a1e7640f92f6af30044
Goooler <wangzongler@gmail.com>
2022-03-20 04:24:55 +0800

    Reformat

--- src/main/kotlin/io/goooler/demoservice/web/HelloController.kt
+++ src/main/kotlin/io/goooler/demoservice/web/HelloController.kt
@@ -3,7 +3,6 @@ package io.goooler.demoservice.web
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
 class HelloController {
 
